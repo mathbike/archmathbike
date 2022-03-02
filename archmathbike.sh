245488a258a287f4ed9de1c1ea130d0bb6b92291
mathbike <mathbike8@gmail.com>
2022-03-02 04:24:45 +0000

    update

--- archmathbike.sh
+++ archmathbike.sh
@@ -7,25 +7,26 @@ pacman -Syu --noconfirm
 # install packages
 pacman -S --noconfirm base-devel linux-headers linux-firmware xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxft libxinerama xclip alsa-utils pulseaudio ttf-jetbrains-mono vim man-db git github-cli gnupg pass passmenu
 # clone dwm, st, dmenu
-git clone git://git.suckless.org/dwm ~/.config/dwm
-git clone git://git.suckless.org/st ~/.config/st
-git clone git://git.suckless.org/dmenu ~/.config/dmenu
+git clone https://github.com/mathbike/dwm.git ~/.config/dwm
+git clone https://github.com/mathbike/st.git ~/.config/st
+git clone https://github.com/mathbike/dmenu.git ~/.config/dmenu
 # make dwm, st, dmenu
 cd ~/.config/dwm && sudo make install
 cd ~/.config/st && sudo make install
-cd ~/.config/dmenu && sudo make install
+cd ~/.config/dmenu && sudo make install && cd
+# clone directories
+git clone https://github.com/mathbike/commands.git
+git clone https://github.com/mathbike/scripts.git
 # clone config files
-
-
-
+git clone --separate-git-dir=$HOME/.dotfiles https://github.com/mathbike/dotfiles.git ~
 # install yay
 git clone https://aur.archlinux.org/yay-git.git ~/.config/yay
 # make yay
-cd yay && makepkg -si --noconfirm
+cd ~/.config/yay && makepkg -si --noconfirm
 # install brave
-yay -S brave-bin
+yay -S brave-bin --noconfirm
 # install lf
-yay -S lf
+yay -S lf --noconfirm
 # install bash-complete-alias
-yay -S bash-complete-alias
+yay -S bash-complete-alias --noconfirm
 
