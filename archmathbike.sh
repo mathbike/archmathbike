db89289c8e134a1c4775d7d0dfb6b82ec0218a5d
mathbike <mathbike8@gmail.com>
2022-03-05 22:35:17 +0000

    update

--- archmathbike.sh
+++ archmathbike.sh
@@ -2,67 +2,93 @@
 
 # archmathbike
 
-# update
-sudo pacman -Syu --noconfirm
-# install packages
-sudo pacman -S --noconfirm base-devel linux-headers linux-firmware xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxft libxinerama xclip alsa-utils pulseaudio ttf-jetbrains-mono gvim man-db git github-cli gnupg pass passmenu bashtop
+# install packages:
+sudo pacman -S --noconfirm \
+	base-devel linux-headers linux-firmware \
+	xorg-server xorg-xinit xorg-xrandr xorg-xsetroot libxft libxinerama xclip \
+	network manager alsa-utils pulseaudio bashtop rsync \
+	ttf-jetbrains-mono gvim man-db git github-cli \
+	gnupg pass passmenu zathura python-pip
 
-git clone https://github.com/mathbike/.dotfiles.git ~/tmpdotfiles
-cd ~/tmpdotfiles
+# delete dotfiles that we already have:
+dotsdelete() {
+	# clone tmpdotfiles
+	git clone https://github.com/mathbike/.dotfiles.git ~/tmpdotfiles && cd ~/tmpdotfiles
+	# list all dotfiles and save to temp txt file, remove . and ..
+	ls -d .* | awk '$1 != "." {print $1}' >> 1.txt
+	cat 1.txt | awk '$1 != ".." {print $1}' >> ~/2.txt && cd ~
+	# declare loop variables
+	local file="2.txt" && local lines=$(cat $file)
+	# run delete loop
+	for line in $lines
+	do
+		rm -rf ${line}
+	done
+	# delete temp files
+	rm -rf 2.txt  ~/tmpdotfiles
+}
 
-# delete dotfiles of the same name as those we already have:
-# list all dotfiles and save to temp txt file, remove . and ..
-ls -d .* | awk '$1 != "." {print $1}' >> 1.txt
-cat 1.txt | awk '$1 != ".." {print $1}' >> ~/2.txt
-# cd home
-cd ~
-# loop variables
-file="2.txt"
-lines=$(cat $file)
-# delete loop
-for line in $lines
-do
-	rm -rf ${line}
-done
-# delete temp txt files
-rm -rf 1.txt 2.txt  ~/tmpdotfiles
+# clone config dotfiles:
+dotfiles() {
+	git clone --bare https://github.com/mathbike/.dotfiles.git ~/.dotfiles
+	# temp config alias
+	alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+	# checkout repo
+	config checkout
+	# do not show untracked files
+	config config --local status.showUntrackedFiles no
+}
 
-# clone config files
-git clone --bare https://github.com/mathbike/.dotfiles.git ~/.dotfiles
-# temp config
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-# checkout repo
-config checkout
-# untracked files
-config config --local status.showUntrackedFiles no
+# clone directories:
+directories() {
+	git clone https://github.com/mathbike/bookmarks.git
+	git clone https://github.com/mathbike/commands.git
+	git clone https://github.com/mathbike/scripts.git	
+}
 
-# clone directories
-git clone https://github.com/mathbike/bookmarks.git
-git clone https://github.com/mathbike/commands.git
-git clone https://github.com/mathbike/scripts.git
-# rename Downloads
-mv Downloads dl
+# install configuration:
+configuration() {
+	# dwm
+	git clone https://github.com/mathbike/dwm.git ~/.config/dwm
+	cd ~/.config/dwm && sudo make install
+	# dwmblocks
+	git clone https://github.com/mathbike/dwmblocks.git ~/.config/dwmblocks
+	cd ~/.config/dwmblocks && sudo make install
+	# dwmstatusbar
+	git clone https://github.com/mathbike/dwmstatusbar.git ~/.config/dwmstatusbar
+	# st
+	git clone https://github.com/mathbike/st.git ~/.config/st
+	cd ~/.config/st && sudo make install
+	# dmenu
+	git clone https://github.com/mathbike/dmenu.git ~/.config/dmenu
+	cd ~/.config/dmenu && sudo make install && cd
+}
 
-# dwm
-git clone https://github.com/mathbike/dwm.git ~/.config/dwm
-cd ~/.config/dwm && sudo make install
-# dwmblocks
-git clone https://github.com/mathbike/dwmblocks.git ~/.config/dwmblocks
-cd ~/.config/dwmblocks && sudo make install
-# st
-git clone https://github.com/mathbike/st.git ~/.config/st
-cd ~/.config/st && sudo make install
-# dmenu
-git clone https://github.com/mathbike/dmenu.git ~/.config/dmenu
-cd ~/.config/dmenu && sudo make install && cd
+# install aur package manager:
+aur() {
+	# yay
+	git clone https://aur.archlinux.org/yay-git.git ~/.config/yay
+	cd ~/.config/yay && makepkg -si --noconfirm
+}
 
-# yay
-git clone https://aur.archlinux.org/yay-git.git ~/.config/yay
-cd ~/.config/yay && makepkg -si --noconfirm
-# brave
-yay -S brave-bin --noconfirm
-rm -rf  ~/.config/BraveSoftware/Brave-Browser/Default
-cp -r  ~/bookmarks/Default ~/.config/BraveSoftware/Brave-Browser
-# lf
-yay -S lf --noconfirm
+# install aur packages:
+aurpackages() {
+	# brave
+	yay -S brave-bin --noconfirm
+	rm -rf  ~/.config/BraveSoftware/Brave-Browser/Default
+	cp -r  ~/bookmarks/Default ~/.config/BraveSoftware/Brave-Browser
+	# lf
+	yay -S lf --noconfirm
+}
+
+# clone symlinks:
+symlinks() {
+
+}
+
+# general housekeeping:
+housekeeping() {
+	# rename Downloads directory
+	rm -rf Downloads && mkdir dl
+}
 
